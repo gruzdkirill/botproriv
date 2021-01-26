a9375828db55324eadc4b9ebd2ada46a7a3b6c4e
gruzdkirill <58915282+gruzdkirill@users.noreply.github.com>
2021-01-26 15:47:54 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot("1501412197:AAHzd30QMMu2UQ5GP2NMbCWurH9ryHLnTD4")
-
+key_M =types.ReplyKeyboardMarkup(True)
+key_M.row('Привет',  'Пока',  'Оло')
 
 @bot.route('/start')
 def start_message(message):
-    bot.send_message(message['chat']['id'], 'Привет, это /start')
+    bot.send_message(message['chat']['id'], 'Привет, это /start',  reply_markup=key_M)
 
 @bot.route('Оло')
-def start_message(message):
+def OLO_message(message):
     bot.send_message(message['chat']['id'], 'Привет, ты написал мне ' + message['text'])
 
 bot.config['api_key'] = '1501412197:AAHzd30QMMu2UQ5GP2NMbCWurH9ryHLnTD4'
